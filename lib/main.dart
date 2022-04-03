f1eff0bf08aad8fab601bef0dfcfe0b8d6f473a6
SirArnoldB <legendaryarnoldb@gmail.com>
2022-04-03 02:06:29 -0500

    added signin progress checking + animation for signin progress

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const SignUpApp());
 
 class SignUpApp extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
   const SignUpApp();
 
   @override
@@ -10,12 +11,15 @@ class SignUpApp extends StatelessWidget {
     return MaterialApp(
       routes: {
         '/': (context) => const SignUpScreen(),
+        // ignore: prefer_const_constructors
+        '/welcome': (context) => WelcomeScreen(),
       },
     );
   }
 }
 
 class SignUpScreen extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
   const SignUpScreen();
 
   @override
@@ -35,6 +39,7 @@ class SignUpScreen extends StatelessWidget {
 }
 
 class WelcomeScreen extends StatelessWidget {
+  // ignore: use_key_in_widget_constructors
   const WelcomeScreen();
 
   @override
@@ -48,6 +53,7 @@ class WelcomeScreen extends StatelessWidget {
 }
 
 class SignUpForm extends StatefulWidget {
+  // ignore: use_key_in_widget_constructors
   const SignUpForm();
 
   @override
@@ -59,15 +65,44 @@ class _SignUpFormState extends State<SignUpForm> {
   final _lastNameTextController = TextEditingController();
   final _usernameTextController = TextEditingController();
 
+  // This methos updates the _formProgress field based on the number of non-empty text fields
+  void _updateFormProgress() {
+    // the progress value is updated in the _updateFormProgress method
+    var progress = 0.0;
+    final controllers = [
+      // when all three fields are filled in, _formProgress is set to 1.0.
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController,
+    ];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress += 1 / controllers.length;
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
+  }
+
+  // ignore: prefer_final_fields
   double _formProgress = 0;
+
+  // Pushes the WelcomeScreen onto the Navigator's stack
+  void _showWelcomeScreen() {
+    Navigator.of(context).pushNamed('/welcome');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          LinearProgressIndicator(value: _formProgress),
+          AnimatedProgressIndicator(value: _formProgress),
           Text('Sign up', style: Theme.of(context).textTheme.headline4),
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -105,10 +140,84 @@ class _SignUpFormState extends State<SignUpForm> {
                     : Colors.blue;
               }),
             ),
-            onPressed: null,
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: const Text('Sign up'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+/// Animation for the LinearProgressIndicator
+///
+/// when the app starts, a tiny red bar appears across the top of the sign in area
+/// when the text field contains text, the red bar turns orange and animates 0.15 of the way across the sign in area
+/// when two text fields contain text, the orange bar turns yellow and animates half of the way across the sign in area
+/// when all three text fields contain text, the orange bar turns green and anmates all the way across the sign in area,
+/// Also, the Sign up Button becomes enabled.
+class AnimatedProgressIndicator extends StatefulWidget {
+  final double value;
+
+  // ignore: use_key_in_widget_constructors
+  const AnimatedProgressIndicator({
+    required this.value,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AnimatedProgressIndicatorState();
+  }
+}
+
+class _AnimatedProgressIndicatorState extends State<AnimatedProgressIndicator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _colorAnimation;
+  late Animation<double> _curveAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        // ignore: prefer_const_constructors
+        duration: Duration(milliseconds: 1200),
+        vsync: this);
+
+    final colorTween = TweenSequence([
+      TweenSequenceItem(
+        tween: ColorTween(begin: Colors.red, end: Colors.orange),
+        weight: 1,
+      ),
+      TweenSequenceItem(
+        tween: ColorTween(begin: Colors.orange, end: Colors.yellow),
+        weight: 1,
+      ),
+      TweenSequenceItem(
+        tween: ColorTween(begin: Colors.yellow, end: Colors.green),
+        weight: 1,
+      ),
+    ]);
+
+    _colorAnimation = _controller.drive(colorTween);
+    _curveAnimation = _controller.drive(CurveTween(curve: Curves.easeIn));
+  }
+
+  @override
+  void didUpdateWidget(oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _controller.animateTo(widget.value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      /// rebuilds the widget tree when the value of an Animation changes
+      animation: _controller,
+      builder: (context, child) => LinearProgressIndicator(
+        value: _curveAnimation.value,
+        valueColor: _colorAnimation,
+        backgroundColor: _colorAnimation.value?.withOpacity(0.4),
       ),
     );
   }
